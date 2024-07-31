6e8dcd08e6f7bf1cbc7029d5dcad02cb04e1f57b
Jason Limas <jasonandrea14@gmail.com>
2024-07-31 18:54:32 +0700

    Add functionality to add items to database

--- app/Http/Controllers/MasterItemController.php
+++ app/Http/Controllers/MasterItemController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MasterItem;
 use Illuminate\Http\Request;
 
 class MasterItemController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'item_code' => 'required|max:4|unique:master_items,item_code',
+            'item_desc' => 'required|max:255',
+            'item_price' => 'required|numeric|min:0'
+        ]);
+
+        $item = MasterItem::create($validated);
+
+        return response()->json(['message' => 'Item created successfully', 'item' => $item], 201);
+    }
 }
--- resources/js/src/views/items/create.vue
+++ resources/js/src/views/items/create.vue
@@ -18,25 +18,23 @@
 
         <div class="row layout-top-spacing">
             <div class="col-xl-12 col-lg-12 col-md-12 col-12 layout-spacing">
-
                 <h1>Tambah Item</h1>
                 <div class="panel br-4">
                     <div class="panel-body">
-                        <!-- Add items form -->
-                        <form>
+                        <form @submit.prevent="submitForm">
                             <div class="form-group">
-                                <input type="text" class="form-control" id="item_code" placeholder="Kode item *" />
+                                <input v-model="item_code" type="text" class="form-control" id="item_code" placeholder="Kode item *" />
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" id="item_desc" placeholder="Deskripsi item *" />
+                                <input v-model="item_desc" type="text" class="form-control" id="item_desc" placeholder="Deskripsi item *" />
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" id="item_price" placeholder="Harga *" />
+                                <input v-model="item_price" type="number" class="form-control" id="item_price" placeholder="Harga *" />
                             </div>
                             <small id="emailHelp2" class="block text-muted">*Harus diisi</small>
-                            <div class="form-group ps-0 mt-3">
-                            </div>
-                            <button type="submit" class="btn btn-primary mt-3">Submit</button>
+                            <div class="form-group ps-0 mt-3"></div>
+                            <button type="submit" class="btn btn-primary mt-3 me-2">Tambah</button>
+                            <a href="/items" class="btn btn-danger mt-3">Batal</a>
                         </form>
                     </div>
                 </div>
@@ -44,13 +42,37 @@
         </div>
     </div>
 </template>
-<style scoped>
-    .layout-px-spacing {
-        min-height: calc(100vh - 170px) !important;
-    }
-</style>
 
 <script setup>
-    import { useMeta } from '@/composables/use-meta';
-    useMeta({ title: 'Blank Page' });
+import { ref } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+const item_code = ref('');
+const item_desc = ref('');
+const item_price = ref('');
+const router = useRouter();
+
+const submitForm = async () => {
+    try {
+        const response = await axios.post('/api/items', {
+            item_code: item_code.value,
+            item_desc: item_desc.value,
+            item_price: item_price.value
+        });
+        console.log(response.data);
+        alert('Item created successfully');
+        // Redirect to the items list page
+        router.push('/items');
+    } catch (error) {
+        console.error('Error response:', error.response);
+        alert('Failed to create item');
+    }
+};
 </script>
+
+<style scoped>
+.layout-px-spacing {
+    min-height: calc(100vh - 170px) !important;
+}
+</style>
